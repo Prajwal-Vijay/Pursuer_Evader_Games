116fb4cbc869fa485cf0dbd4cf468d9e599a0c7c
Prajwal-Vijay <prajvi.10@gmail.com>
2025-08-26 22:45:02 +0530

    implemented evader init check completely

--- Research/Code/Environment.py
+++ Research/Code/Environment.py
@@ -8,7 +8,8 @@ import Pursuer
 import Evader
 import time
 import random
-
+import cvxpy as cp
+from itertools import combinations
 
 class Environment:
     def __init__(self, N, M, t, pursuers, evaders):
@@ -41,10 +42,63 @@ class Environment:
             Do we really have to calculate for all the coalitions now itself, or can we consider them seperately and calculate? No we need to include all the 
             pursuers coalitions and try to calculate, because it may happen individually the evader cannot be caught, but as a group it can be.
         """
-        
-
+        evasion_matrix, coalition_list = self.create_evasion_matrix()
+        for evader_idx, evader in enumerate(self.evaders):
+            for coalition in coalition_list:
+                if evasion_matrix[(evader_idx, coalition)] == 1:
+                    # There exists atleast one coalition which can successfully capture the evader.
+                    break 
+            else:
+                # There exists an evader that always manages to escape!
+                return False
         return True
 
+    def create_evasion_matrix(self, max_coalition_size=3):
+        # Generate all possible pursuer coalitions up to max_coalition_size
+        coalition_list = []
+        for size in range(1, min(max_coalition_size + 1, len(self.pursuers) + 1)):
+            for coalition in combinations(range(len(self.pursuers)), size):
+                coalition_list.append(coalition)
+        
+        evasion_matrix = {}
+        
+        # For each evader and each pursuer coalition
+        for evader_idx, evader in enumerate(self.evaders):
+            for coalition in coalition_list:
+                min_z = self._compute_min_z_in_bes(evader_idx, coalition)
+                
+                # Set matrix value: 1 if min_z > 0, -1 otherwise
+                evasion_matrix[(evader_idx, coalition)] = 1 if min_z > 0 else -1
+        return evasion_matrix, coalition_list
+
+    def _compute_min_z_in_bes(self, evader_idx, coalition):
+        # Uses the boundary of evasion space method as given in the paper.
+        evader = self.evaders[evader_idx]
+        x = cp.Variable(3)
+        objective = cp.Minimize(x[2])
+        constraints = []
+        for pursuer_idx in coalition:
+            pursuer = self.pursuers[pursuer_idx]
+            evader_pos = evader.get_pos()
+            pursuer_pos = pursuer.get_pos()
+            
+            alpha_ij = pursuer.speed/evader.speed
+            capture_radius = pursuer.capture_radius
+            
+            dist_to_pursuer = cp.norm(x - pursuer_pos, 2)
+            dist_to_evader = cp.norm(x - evader_pos, 2)
+            constraints.append(dist_to_pursuer - alpha_ij*dist_to_evader >= capture_radius)
+        problem = cp.Problem(objective, constraints)
+        try:
+            problem.solve(solver=cp.ECOS, verbose=False)
+            
+            if problem.status == cp.OPTIMAL:
+                return problem.value
+            else:
+                return -np.inf
+        except:
+            return -np.inf
+            
     def plot_current_positions(self):
         """Plot current positions of pursuers and evaders"""
         import matplotlib.pyplot as plt
